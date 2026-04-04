9d7269666bb3a650bf2f98faa6b90b785308bca0
cyberwork3 <cyberwork.revanth@gmail.com>
2026-04-04 12:09:50 +0530

    new java file from anti-gravity

---
+++ sum.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 32;
+        System.out.println("sum of a and b is: " + (a + b));
+    }
+}
+
+// my first java code after years
